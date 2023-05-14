a7c99d2460ddf67ec9ed5b3a5d383cf776f16c92
anaserrami <ramianas314@gmail.com>
2023-05-14 13:30:03 +0100

    update code

--- app.js
+++ app.js
@@ -3,8 +3,11 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var articlesRouter = require('./routes/articles');
+var categoriesRouter = require('./routes/categories');
+var commentairesRouter = require('./routes/commentaires');
 
 var app = express();
 
@@ -14,7 +17,10 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/articles', articlesRouter);
+app.use('/categories', categoriesRouter);
+app.use('/commentaires', commentairesRouter);
 
 module.exports = app;
